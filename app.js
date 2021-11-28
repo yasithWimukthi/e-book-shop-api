a5badfa3728ec0d8c04558f68d359f331535afa8
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-28 11:28:44 +0530

    create directories

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRouter = require('./routes/user');
+
 require('dotenv').config()
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('node')
-})
+app.use('/api/user',userRouter)
 
-const post = process.env.PORT || 8000;
 
-app.listen(post,()=>{
-    console.log('server listening on port 8000')
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=>{
+    console.log('database connected')
+    const post = process.env.PORT || 8000;
+
+    app.listen(post,()=>{
+        console.log('server listening on port 8000')
+    })
 })
+    .catch(err => {
+        console.log(err);
+    });
+
+
 
