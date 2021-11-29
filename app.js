e008b14c00762b69ad5ff956d3119bd43e0fe547
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-29 09:00:04 +0530

    implement sign in

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
 //routes
 app.use('/api/user',userRouter)
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 const User = require('../models/user');
 const HttpError = require('../models/http-error');
@@ -90,3 +91,73 @@ exports.signup = async (req,res, next) => {
             history:createdUser.history,
             token: token });
 };
+
+
+exports.signin= async (req,res, next) =>{
+    const { email, password } = req.body;
+
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!existingUser) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.',
+            401
+        );
+        return next(error);
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        const error = new HttpError(
+            'Could not log you in, please check your credentials and try again.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.',
+            401
+        );
+        return next(error);
+    }
+
+    //generate token
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+        // persist token as cookie
+        res.cookie('token',token,{expire:new Date()+9999});
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    res.json({
+        userId: existingUser.id,
+        name: existingUser.name,
+        email: existingUser.email,
+        role:existingUser.role,
+        token: token
+    });
+}
